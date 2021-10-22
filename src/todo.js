1f5a0bbd599e1baa8d3ae1c4027942e213400b6d
Mikael Larsson <mikael.j.larsson@hotmail.com>
2021-10-22 14:32:19 +0200

    Added todo

---
+++ src/todo.js
@@ -0,0 +1,9 @@
+/**
+ * @todo Add static elements to the view
+ * @todo Add basic styling to the elements
+ * @todo Make the comopnent stateful
+ * @todo Add types
+ * @todo Add Redux state management
+ * @todo Add unit/snapshot tests
+ * @todo Add integration test with Cypress
+ */
