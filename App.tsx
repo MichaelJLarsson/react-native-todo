146a36c03678d7433ead3fc11311008b3aa7fc1d
Mikael Larsson <mikael.j.larsson@hotmail.com>
2021-10-24 15:49:03 +0200

    Added basic styling

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import {Todo} from './src/Todo.jsx';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout } from '@ui-kitten/components';
+import { Todo } from './src/Todo.jsx';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Todo />
-      <StatusBar style="auto" />
-    </View>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Layout style={styles.container}>
+        <Todo />
+        <StatusBar style="auto" />
+      </Layout>
+    </ApplicationProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 100,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,5 +1,7 @@
-import React, {useState} from 'react';
-import { Text, View, TextInput, Button, FlatList } from 'react-native';
+import React, { useState } from 'react' ;
+import { View, StyleSheet } from 'react-native';
+import { Text, Button, Divider, List, ListItem, Input } from '@ui-kitten/components';
+import { Icon } from 'react-native-eva-icons';
 
 export const Todo = () => {
     const [text, setText] = useState('')
@@ -18,28 +20,79 @@ export const Todo = () => {
             text: 'Do something else'
         }
     ];
+    
+    const TodoListItem = ({ item }) => (
+        <ListItem title={`${item.text} ${item.id}`} />
+    );
 
-    const Item = ({ text }) => (
-        <View>
-            <Text>{ text }</Text>
-        </View>
+    const PlusIcon = () => (
+        <Icon
+            name='plus-circle-outline'
+            fill='#ffffff'
+            style={styles.plusIcon}
+        />
     );
-    
-    const TodolistItem = ({ item }) => (
-        <Item title={item.text} />
-    );
-    
+
     return (
-        <View>
-            <Text>Welcome to todo app!</Text>
+        <View style={styles.todoContainer}>
+            <View style={styles.header}>
+                <Text category='h3'>Welcome to the todo app!</Text>
+            </View>
             
             {/* Add a new todo item */}
-            <Text>Add a new todo</Text>
-            <TextInput type="string" defaultValue={text} onChangeText={text => setText(text)} placeholder="Add your todo" />
-            <Button title="Press my button!" />
+            <Text category='label'>Add a new todo</Text>
+            <View style={[
+                styles.addTodoForm
+                // ,debug.blueDashedBorder
+            ]}>
+                <Input value={text}  placeholder="What do you want to do?" onChangeText={text => setText(text)} style={styles.todoInput} />
+                <Button accessoryLeft={PlusIcon}>Add it!</Button>
+            </View>
 
             {/* List all the todo items */}
-            <FlatList data={todoItems} renderItem={TodolistItem} keyExtractor={item => item.id} />
+            <List
+                style={styles.todoListContainer}
+                data={todoItems}
+                ItemSeparatorComponent={Divider}
+                renderItem={TodoListItem}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    todoContainer: {
+        width: '100%',
+    },
+    header: {
+        alignItems: 'center'
+    },
+    plusIcon: {
+        width: 24,
+        height: 24
+    },
+    todoListContainer: {
+        maxHeight: 200,
+    },
+    addTodoForm: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexGrow: 1,
+    },
+    todoInput: {
+        flexGrow: 1
+    }
+});
+
+const debug = StyleSheet.create({
+    blueDashedBorder: {
+        borderStyle: 'dashed',
+        borderColor: '#00F',
+        borderWidth: 1
+    },
+    redDashedBorder: {
+        borderStyle: 'dashed',
+        borderColor: '#F00',
+        borderWidth: 1
+    }
+})
